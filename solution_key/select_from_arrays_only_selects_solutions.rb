be48b9131cedf4deb30799f97564627922ddb8dd
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-10 10:36:52 -0500

    Update select_from_arrays_only_selects_solutions.rb
    add 5th practice solution

--- solution_key/select_from_arrays_only_selects_solutions.rb
+++ solution_key/select_from_arrays_only_selects_solutions.rb
@@ -38,6 +38,13 @@ p even_numbers
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
+strings_array = ["a", "man", "a", "plan", "a", "canal", "panama"]
+shorter_array = strings_array.select do |select|
+  select.length < 4
+end
+
+p shorter_array
+
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
