4dbad8919d1617c1086aba1a6e54c731399fc47d
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-10 10:32:43 -0500

    Update select_from_arrays_only_selects_solutions.rb
    add 3rd practice problem

--- solution_key/select_from_arrays_only_selects_solutions.rb
+++ solution_key/select_from_arrays_only_selects_solutions.rb
@@ -20,7 +20,11 @@ p w_strings
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
-
+hashs = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+prices_hashs = hashs.select do |select|
+  select[:price] > 5
+end
+p prices_hashs
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
