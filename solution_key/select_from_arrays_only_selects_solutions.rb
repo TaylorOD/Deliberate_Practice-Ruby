edb7feeae94f32ddef20b7a824517fe6a6f758af
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-10 10:39:37 -0500

    Update select_from_arrays_only_selects_solutions.rb
    add 7th practice problem

--- solution_key/select_from_arrays_only_selects_solutions.rb
+++ solution_key/select_from_arrays_only_selects_solutions.rb
@@ -57,6 +57,12 @@ p new_array
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
+numbers_array = [8, 23, 0, 44, 1980, 3]
+new_array = numbers_array.select do |select|
+  select < 10
+end
+p new_array
+
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
