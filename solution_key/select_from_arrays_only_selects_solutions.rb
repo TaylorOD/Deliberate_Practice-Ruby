e95fc0d128f1b1101adb296fe53ae09b5731dda3
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-10 10:29:29 -0500

    Update select_from_arrays_only_selects_solutions.rb
    add 1st problem solution

--- solution_key/select_from_arrays_only_selects_solutions.rb
+++ solution_key/select_from_arrays_only_selects_solutions.rb
@@ -1,6 +1,13 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+numbers_array = [2, 32, 80, 18, 12, 3]
+less_than_twenty = numbers_array.select do |twenty|
+  twenty < 20
+end
+
+p less_than_twenty
+
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
