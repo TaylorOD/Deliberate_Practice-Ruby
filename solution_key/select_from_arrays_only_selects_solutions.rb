63181c08ee5e35683ce7d25d7e04559226a637bb
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-10 10:30:59 -0500

    Update select_from_arrays_only_selects_solutions.rb
    add 2nd practice problem

--- solution_key/select_from_arrays_only_selects_solutions.rb
+++ solution_key/select_from_arrays_only_selects_solutions.rb
@@ -11,8 +11,16 @@ p less_than_twenty
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
+strings_array = ["winner", "winner", "chicken", "dinner"]
+w_strings = strings_array.select do |select|
+  select[0] == "w"
+end
+p w_strings
+
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
