4346c398e3774aa9f91a2e7798d4453cee069d0d
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-21 10:14:48 -0500

    Update nested_loops_practice_solutions.rb
    - add solution two

--- solution_key/nested_loops_practice_solutions.rb
+++ solution_key/nested_loops_practice_solutions.rb
@@ -20,6 +20,23 @@ p flat_array
 #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
+string1 = ["a", "b", "c"]
+string2 = ["d", "e", "f", "g"]
+combination_strings = []
+
+index1 = 0
+while index1 < string1.length
+  index2 = 0
+  while index2 < string2.length
+    combination_strings << string1[index1] + string2[index2]
+    index2 += 1
+  end
+
+  index1 += 1
+end
+
+p combination_strings
+
 #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
