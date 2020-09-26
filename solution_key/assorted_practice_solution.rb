c07400bfda61d13a19a651a58814845b3d19379e
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-25 22:28:35 -0500

    Update assorted_practice_solution.rb
    - add 7th question

--- solution_key/assorted_practice_solution.rb
+++ solution_key/assorted_practice_solution.rb
@@ -70,4 +70,15 @@ end
 
 p strings
 
+#  7. Start with an array of numbers and create a new array with only the numbers less than 20.
+#     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+numbers = [2, 32, 80, 18, 12, 3]
+
+less_than_twenty = numbers.select do |number|
+  number < 20
+end
+
+p less_than_twenty
+
 
