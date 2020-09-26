89bea2620ed0bf69ef6296d1cf5a92414a6b8fbd
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-25 22:32:23 -0500

    Update assorted_practice_solution.rb
    - add 8th wuestion

--- solution_key/assorted_practice_solution.rb
+++ solution_key/assorted_practice_solution.rb
@@ -81,4 +81,13 @@ end
 
 p less_than_twenty
 
+#  8. Start with an array of numbers and create a new array with only the even numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+even_numbers = numbers.select do |number|
+  number % 2 == 0
+end
+p even_numbers
+
 
