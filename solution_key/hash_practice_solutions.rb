175ccd75dd5cc7989fecaf75287f6b29332afe17
Taylor Dorsett <taylorsunwantedemails@gmail.com>
2020-09-30 13:35:44 -0500

    Update hash_practice_solutions.rb
    - add 4th solution

--- solution_key/hash_practice_solutions.rb
+++ solution_key/hash_practice_solutions.rb
@@ -16,6 +16,14 @@ puts full_names[0]
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
+menu = {
+  burger: 5.00,
+  fries: 3.50,
+  apple: 1.50,
+}
+menu[:pie] = 5
+p menu
+
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
